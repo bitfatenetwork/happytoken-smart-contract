8c9026427dfc398d5b261f73a314595611f7485c
Bitfate <144492532+bitfatenetwork@users.noreply.github.com>
2024-01-22 19:41:25 +0530

    Update HappyToken.sol

--- contracts/HappyToken.sol
+++ contracts/HappyToken.sol
@@ -1,63 +1,18 @@
-// SPDX-License-Identifier: Apache-1.0
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-contract HappyToken {
-    string public name     = "Happy";
-    string public symbol   = "HAP";
-    uint8  public decimals = 18;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    event  Approval(address indexed src, address indexed guy, uint wad);
-    event  Transfer(address indexed src, address indexed dst, uint wad);
-    event  Deposit(address indexed dst, uint wad);
-    event  Withdrawal(address indexed src, uint wad);
-
-    mapping (address => uint)                       public  balanceOf;
-    mapping (address => mapping (address => uint))  public  allowance;
-
-    function a() public payable {
-        deposit();
-    }
-    function deposit() public payable {
-        balanceOf[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
-    }
-    function withdraw(uint wad) public {
-        require(balanceOf[msg.sender] >= wad);
-        balanceOf[msg.sender] -= wad;
-        msg.sender.transfer(wad);
-        emit Withdrawal(msg.sender, wad);
-    }
-
-    function totalSupply() public view returns (uint) {
-        return address(this).balance;
-    }
-
-    function approve(address guy, uint wad) public returns (bool) {
-        allowance[msg.sender][guy] = wad;
-        emit Approval(msg.sender, guy, wad);
-        return true;
-    }
-
-    function transfer(address dst, uint wad) public returns (bool) {
-        return transferFrom(msg.sender, dst, wad);
-    }
-
-    function transferFrom(address src, address dst, uint wad)
-    public
-    returns (bool)
+/// @custom:security-contact info@bitfate.com
+contract BitfateProtocol is ERC20, ERC20Burnable, ERC20Permit, Ownable {
+    constructor(address initialOwner)
+        ERC20("Bitfate Protocol", "BITF")
+        ERC20Permit("Bitfate Protocol")
+        Ownable(initialOwner)
     {
-        require(balanceOf[src] >= wad);
-
-        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
-            require(allowance[src][msg.sender] >= wad);
-            allowance[src][msg.sender] -= wad;
-        }
-
-        balanceOf[src] -= wad;
-        balanceOf[dst] += wad;
-
-        Transfer(src, dst, wad);
-
-        return true;
+        _mint(msg.sender, 700000 * 10 ** decimals());
     }
 }
